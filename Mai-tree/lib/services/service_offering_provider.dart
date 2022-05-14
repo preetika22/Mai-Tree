26e09450a0e1f74958ceaa4ffd8d8d3fd94a1640
mrinaldas <3842015+mrinaldas@users.noreply.github.com>
2022-05-14 01:45:11 +0100

    Moved json file in assets folder

--- Mai-tree/lib/services/service_offering_provider.dart
+++ Mai-tree/lib/services/service_offering_provider.dart
@@ -6,7 +6,7 @@ import '../view_models/service_offering.dart';
 class ServiceOfferingProvider{
   Future<List<ServiceOffering>> fetchServiceOfferings() async {
     // TODO: This will eventually be replaced with an http call
-    final String response = await rootBundle.loadString('services/service_offerings.json');
+    final String response = await rootBundle.loadString('assets/data/service-offerings.json');
 
     final jsonData = json.decode(response);
     return List<ServiceOffering>.from(jsonData.map((x) => ServiceOffering.fromJson(x)));
